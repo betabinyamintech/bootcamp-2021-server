0734f40a255fe0f2e9d56ed91455f5e2b73a62c2
Tamar Levanoni <tamari250@gmail.com>
2021-03-16 13:13:49 +0200

    add inquiry schema&update user schema

--- test.js
+++ test.js
@@ -7,3 +7,13 @@ request.post(serverUrl + '/login')
         .send({email: "a", password: "b"})
         .then(result => 
             console.log(result.body.token))
+request.post(serverUrl + '/hi')
+        .set('Accept', 'application/json')
+        .send({email: "a", password: "b"})
+        .then(result => 
+            console.log(result.body.token))
+request.post(serverUrl + '/login')
+        .set('Accept', 'application/json')
+        .send({email: "a", password: "b"})
+        .then(result => 
+            console.log(result.body.token))
