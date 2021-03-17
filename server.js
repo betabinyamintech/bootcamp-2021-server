25c8157e9da07f722c5e51d8af5fa2a84e11734d
Yehonatan Levi <leviyehonatan@gmail.com>
2021-03-17 15:17:57 +0200

    port 80 by default

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //server.js
 const app = require("./app");
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT | 80, () => {
   console.log("Opened port succesfully at port " + process.env.PORT);
 });
