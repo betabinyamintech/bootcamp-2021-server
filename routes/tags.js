941ad6a6126a6c03641fac8cc1e747bb3ad41af2
ReutAttar <32901809+ReutAttar@users.noreply.github.com>
2021-03-17 14:51:36 +0200

    add tags CRUD

--- routes/tags.js
+++ routes/tags.js
@@ -4,28 +4,23 @@ var router = express.Router();
 const {
   models: { Tag },
 } = require("../models");
-/* GET users listing. */
 
-router.get("/:name",authenticateToken, async (req, res) => {
-  const { name } = req.params;
-  const tag = await Tag.findOne({ name }).exec();
-  res.send(tag);
+router.get("/", authenticateToken, async (req, res) => {
+  const tagsList = await Tag.find({}).exec();
+  res.send(tagsList);
 });
-router.get("/",authenticateToken, async (req, res) => {
-  const tag = await Tag.find().exec();
-  res.send(tag);
-});
-router.post("/",authenticateToken, async (req, res) => {
-  const { name } = req.body;
-  const tag = await new Tag({ name }).save();
-  console.log("POST!", tag);
-  res.send(tag);
-});
-router.delete("/",authenticateToken, async (req, res) => {
-  const { name } = req.body;
-  await Tag.deleteOne({ name }).exec();
 
-  res.send("OK!");
+// router.get("/:name", authenticateToken, async (req, res) => {
+//   const { name } = req.params;
+//   const tag = await Tag.findOne({ name }).exec();
+//   res.send(tag);
+// });
+
+router.post("/", authenticateToken, async (req, res) => {
+  // const { name } = req.body;
+  const tag = await new Tag(req.body).save();
+  console.log("POST! ", tag);
+  res.send(tag);
 });
 
 module.exports = router;
